88b530d651a943b0070e9548e807e02d38d5884b
Mahima de Silva <mahimaindhot@gmail.com>
2023-03-22 21:07:46 +0530

    built after changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,6 @@ import cityList from '../assets/cities.json';
 export const environment = {
   production: true,
   WEATHER_API_KEY: 'e4e5a640da20f3a05005a29745eda68c',
-  CITY_CODE_LIST: cityList.List.map((city) => city.CityCode).slice(0, 3),
+  CITY_CODE_LIST: cityList.List.map((city) => city.CityCode),
   API_ENDPOINT: `https://api.openweathermap.org/data/2.5/weather?id={%ID%}&appid={%APIKEY%}`,
 };
